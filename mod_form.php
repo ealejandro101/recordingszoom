d21e274f6b734b2b19709883444016a679bcba54
Alejandro Escobar <ealejandro101@gmail.com>
2018-07-17 12:55:40 -0500

    accesos

--- mod_form.php
+++ mod_form.php
@@ -51,7 +51,7 @@ class mod_recordingszoom_mod_form extends moodleform_mod {
         // Adding the "general" fieldset, where all the common settings are showed.
         $mform->addElement('header', 'general', get_string('general', 'form'));
 
-        if( !is_siteadmin( $USER) ){
+        if( !is_siteadmin( $USER->id) ){
 
         } else {
 
