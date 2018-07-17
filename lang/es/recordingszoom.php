7f326ec6bb7b5128eaf69d1aa206d2799e64cc60
Alejandro Escobar <ealejandro101@gmail.com>
2018-07-17 14:59:33 -0500

    idioma

---
+++ lang/es/recordingszoom.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for recordingszoom
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_recordingszoom
+ * @copyright  2018 Alejandro Escobar <ealejandro101@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['modulename'] = 'Grabaciones Zoom';
+$string['modulenameplural'] = 'Grabaciones Zoom Vinculadas';
+$string['modulename_help'] = 'Use este modulo para vincular las grabaciones de reuniones Zoom a su curso | El modulo permite vincular y listar las grabaciones de una reunión Zoom mediante el ID de reunión';
+$string['apiurl'] = 'Zoom URL Api';
+$string['apiurl_desc'] = 'URL entregada por Zoom para los servicios del API';
+$string['apikey'] = 'Zoom apikey';
+$string['apikey_desc'] = 'Clave entregada por Zoom para el uso del API';
+$string['apisecret'] = 'Zoom apisecret';
+$string['apisecret_desc'] = 'Clave entregada por Zoom para el uso del API';
+$string['pagetitle'] = 'Lista de grabaciones vinculadas con reunión de Zoom';
+$string['recordingszoom:addinstance'] = 'Adicionar nuevas grabaciones Zoom';
+$string['recordingszoom:submit'] = 'Enviar Grabaciones Zoom';
+$string['recordingszoom:view'] = 'Ver Grabaciones Zoom';
+$string['recordingszoomname'] = 'Topic';
+$string['recordingszoomname_help'] = 'Tema asociado con la reunión de zoom y el aula moodle.';
+$string['recordingszoom'] = 'Grabaciones Zoom';
+$string['pluginadministration'] = 'Administración Grabaciones Zoom';
+$string['zoommeetingid'] = 'Zoom meeting ID';
+$string['zoommeetingid_help'] = 'ID de la reunión en Zoom, usualmente es un numero de 10 digitos';
+$string['falla_zoommeetingid'] = 'El ID de la reunión en Zoom es necesario para realizar la vinculación';
+$string['fechainicialbusqueda'] = 'Fecha inicial de busqueda';
+$string['topic'] = 'Tema';
+$string['starttime'] = 'Fecha de inicio';
+$string['duration'] = 'Duración';
+$string['accion'] = 'Acción';
+$string['playurl'] = 'Url de reproducción';
+$string['titulodelalista'] = 'Lista de grabaciones para: ' ;
+$string['playrecording'] = 'Ver grabación' ;
+
